cbbdc48a6e472d7fb34f1798b066d40d7ebec4ff
April Hall <ari@arithefirst.com>
2025-01-31 19:40:41 -0500

    feat: Test sending requests with Cloudflare SDK

--- main.go
+++ main.go
@@ -1,20 +1,27 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
+
+	"github.com/cloudflare/cloudflare-go/v3/zones"
 )
 
 func main() {
-	ip, err := getIpAddress()
-	if err != nil {
-		log.Fatal(err)
-	}
+	// ip, err := getIpAddress()
+	// if err != nil {
+	// 	log.Fatal(err.Error())
+	// }
 
 	client, err := getClient()
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal(err.Error())
 	}
 
-	fmt.Printf("IP: %sClient: %v\n", ip, client)
+	page, err := client.Zones.List(context.TODO(), zones.ZoneListParams{})
+	if err != nil {
+		log.Fatalf("Error in request: %s", err.Error())
+	}
+	fmt.Printf("%v\n", page)
 }
