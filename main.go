9ea1569547fe75e123a59c2b90b69b22f7225f79
April Hall <ari@arithefirst.com>
2025-02-01 00:25:43 -0500

    feat: Get IDs for domains in config

--- main.go
+++ main.go
@@ -5,13 +5,15 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/cloudflare/cloudflare-go/v3"
+	"github.com/cloudflare/cloudflare-go/v3/dns"
 	"github.com/cloudflare/cloudflare-go/v3/zones"
 )
 
 type config struct {
-	Token string
-	Email string
-	Zones []string
+	Token   string
+	Email   string
+	Records []string
 }
 
 func main() {
@@ -26,16 +28,31 @@ func main() {
 		log.Fatal(err.Error())
 	}
 
+	fmt.Print(Config)
+
 	print("Creating Client...\n")
 	client, err := getClient(Config)
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 
-	print("Sending Request...\n")
-	page, err := client.Zones.List(context.TODO(), zones.ZoneListParams{})
-	if err != nil {
-		log.Fatalf("Error in request: %s", err.Error())
+	// Fetch all of your account's Zone IDs
+	zones, err := client.Zones.List(context.TODO(), zones.ZoneListParams{})
+	// For each Zone ID, search for the requested domains
+	for _, zone := range zones.Result {
+		page, err := client.DNS.Records.List(context.TODO(), dns.RecordListParams{
+			ZoneID: cloudflare.F(zone.ID),
+		})
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+
+		for _, record := range Config.Records {
+			for _, v := range page.Result {
+				if v.Name == record {
+					fmt.Print("AHH")
+				}
+			}
+		}
 	}
-	fmt.Printf("%v\n", page)
 }
